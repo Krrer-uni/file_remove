611ea4e9797dcf862ef0243d854b589f3d369799
Krrer-uni <JaramBlanta420XD>
2021-10-15 13:44:58 +0200

    FileRename.java added

---
+++ src/main/java/file/rename/FileRename.java
@@ -0,0 +1,16 @@
+package file.rename;
+
+import java.io.*;
+
+public class FileRename {
+    public static void main(final String[] args) {
+            RenameProcess process = new RenameProcess();
+            ResultReader reader = new ResultReader();
+        try {
+            process.rename();
+            reader.print(process.getInput());
+            reader.print(process.getError());
+        } catch (IOException e) { }
+
+    }
+}
